57cee077f664e4d7871ec67f47ea756409f4de8f
Alon Ribeiro <56462713+AlonRibeiro12@users.noreply.github.com>
2021-06-16 15:08:57 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var cores = ['Vermelho', 'Verde', 'Azul', 'Roxo', 'Rosa', 'Amarelo', 'Branco', 'Preto']
+
+//Join(). Junta elementos e adiciona algo entre eles console.log(cores.join(' -'));
+
+//Reverse(). serve para inverter a ordem dos elementos console.log(cores.reverse().join(' -'));
+
+//sort(). apresenta os elementos do array em ordem alfabética console.log(cores.sort().join(' -'));
